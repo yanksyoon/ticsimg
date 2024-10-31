2e3a0e67bc90f3322a9a24b9c1d41c2e608bd58b
azkaratea <164136436+azkaratea@users.noreply.github.com>
2024-10-31 16:31:16 +0100

    Update tics_installer.sh

--- tics_installer.sh
+++ tics_installer.sh
@@ -21,23 +21,25 @@ apt-get install -y \
     pylint
 
 # Set up a new user
-UID=1001
-GID=1001
-UNAME="runner"
-groupadd -g $GID -o $UNAME
-useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME
-echo "$UNAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$UNAME
-chmod 0440 /etc/sudoers.d/$UNAME
+USRID=1001
+GRPID=1001
+USRNAME="runner"
+groupadd -g $GRPID -o $USRNAME
+useradd -m -u $USRID -g $GRPID -o -s /bin/bash $USRNAME
+echo "$USRNAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$USRNAME
+chmod 0440 /etc/sudoers.d/$USRNAME
 
 # Switch to the new user
-sudo -i -u $UNAME bash << EOF
+sudo -i -u $USRNAME bash << EOF
 
-# TICSAUTHTOKEN need to be exported to the environment
+#export TICSAUTHTOKEN - we need this to be injected to the user environment
 
 # Download and install TiCS
-curl -o /home/$UNAME/install_tics.sh -L "https://canonical.tiobe.com/tiobeweb/TICS/api/public/v1/fapi/installtics/Script?cfg=default&platform=linux&url=https://canonical.tiobe.com/tiobeweb/TICS/"
-chmod +x /home/$UNAME/install_tics.sh
+curl -o /home/$USRNAME/install_tics.sh -L "https://canonical.tiobe.com/tiobeweb/TICS/api/public/v1/fapi/installtics/Script?cfg=default&platform=linux&url=https://canonical.tiobe.com/tiobeweb/TICS/"
+chmod +x /home/$USRNAME/install_tics.sh
 ./install_tics.sh
+
+source /home/$USRNAME/.profile
 
 # Run additional TiCS maintenance commands
 TICSMaintenance -checkchk
